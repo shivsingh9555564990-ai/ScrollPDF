494f73685d7cd315cf6af7372e8953e443fa41f4
shivsingh9555564990-ai <shivsingh9555564990@gmail.com>
2026-06-18 15:27:59 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-const nextConfig = {
-  output: "export",      // ⭐ important (static mode)
-  images: {
-    unoptimized: true,    // Next Image issue fix
-  },
-  trailingSlash: true,
-};
-
-module.exports = nextConfig;
